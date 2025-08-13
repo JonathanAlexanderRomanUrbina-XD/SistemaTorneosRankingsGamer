2dbe6407fb78daf7374410e1d6f1539d64a9cf4e
Jonatan <ru25001@esfe.agape.edu.sv>
2025-08-13 08:26:28 -0600

    primer commit

---
+++ torneosrankings/src/main/java/com/example/torneosrankings/TorneosrankingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.torneosrankings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorneosrankingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorneosrankingsApplication.class, args);
+	}
+
+}
---
+++ torneosrankings/src/test/java/com/example/torneosrankings/TorneosrankingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.torneosrankings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorneosrankingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
